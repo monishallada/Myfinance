417a31ce356862221c1c023c99ddf84ab0200234
Bhavesh-el <108587187+Bhavesh-el@users.noreply.github.com>
2025-07-10 16:07:05 +0000

    should

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MyFinanceLanding from './MyFinanceLanding';
+import Dashboard from './Dashboard'; // your other page
 
 function App() {
   return (
-    <div>
-      <MyFinanceLanding />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MyFinanceLanding />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
+
+
